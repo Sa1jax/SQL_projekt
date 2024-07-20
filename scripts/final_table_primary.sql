9fbfb8332a288b836e29c775035c52d3122ef7ba
David Hrubý <hrubyd74@gmail.com>
2024-07-20 14:35:50 +0200

    Přidej dočasnou tabulku s cenami potravin během let

--- scripts/final_table_primary.sql
+++ scripts/final_table_primary.sql
@@ -19,3 +19,18 @@ JOIN czechia_payroll_industry_branch cpi
 	AND calculation_code = 100
 GROUP BY cpi.name, cp.payroll_year 
 ORDER BY cpi.name, cp.payroll_year;
+
+
+-- Dočasná tabulka s cenami potravin za daný rok
+CREATE TEMPORARY TABLE temp_grocery_prices AS
+SELECT
+	YEAR(cp.date_from) AS year_,
+	cpc.name AS food,
+	ROUND(AVG(cp.value), 2) AS price,
+	cpc.price_value AS amount,
+	cpc.price_unit AS unit
+FROM czechia_price cp
+JOIN czechia_price_category cpc
+	ON cp.category_code = cpc.code
+GROUP BY cpc.name, YEAR(cp.date_from)
+ORDER BY year_;
